1b8964d05b90e24e69aefe57503b014dbe91be2d
gavd <gustavo@gavd.net>
2018-12-02 21:23:11 +0100

    File added

---
+++ src/webparts/prac365BingSpellCheckWp/Prac365BingSpellCheckWpWebPart.module.scss.ts
@@ -0,0 +1,21 @@
+/* tslint:disable */
+require('./Prac365BingSpellCheckWpWebPart.module.css');
+const styles = {
+  controlRow: 'controlRow_8c5bd0cb',
+  controlLabel: 'controlLabel_8c5bd0cb',
+  buttonRow: 'buttonRow_8c5bd0cb',
+  resultRow: 'resultRow_8c5bd0cb',
+  prac365BingSpellCheckWp: 'prac365BingSpellCheckWp_8c5bd0cb',
+  container: 'container_8c5bd0cb',
+  row: 'row_8c5bd0cb',
+  column: 'column_8c5bd0cb',
+  'ms-Grid': 'ms-Grid_8c5bd0cb',
+  title: 'title_8c5bd0cb',
+  subTitle: 'subTitle_8c5bd0cb',
+  description: 'description_8c5bd0cb',
+  button: 'button_8c5bd0cb',
+  label: 'label_8c5bd0cb',
+};
+
+export default styles;
+/* tslint:enable */
